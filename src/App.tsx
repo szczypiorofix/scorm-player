efe9340620805babcebcb9f308f9781dd052c63d
szczypiorofix <szczypiorofix@o2.pl>
2025-07-22 08:44:26 +0200

    Fix useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ScormPlayer from './ScormPlayer';
 import { useScormManifest } from "./hooks/useScormManifest";
@@ -6,6 +6,7 @@ import { useScormManifest } from "./hooks/useScormManifest";
 import './App.scss';
 
 const App: React.FC = () => {
+    const [parsed, setParsed] = useState(false);
     const {
         loading,
         error,
@@ -14,17 +15,23 @@ const App: React.FC = () => {
     } = useScormManifest();
 
     useEffect(() => {
-        parseManifestFromUrl('/scorm/imsmanifest.xml')
-            .then(() => console.log('Manifest parsed'))
-            .catch(e => console.log('Error: ' + e));
-    }, []);
+        if (!parsed) {
+            parseManifestFromUrl('/scorm/imsmanifest.xml')
+                .then(() => {
+                    console.log('Manifest parsed');
+                    setParsed(true);
+                })
+                .catch(e => console.log('Error: ' + e));
+        }
+    }, [parsed, parseManifestFromUrl]);
 
     const launchUrl = getLaunchUrl();
-    console.log(launchUrl);
 
     if (loading) return <div>Loading SCORM package...</div>;
     if (error) return <div>Error: {error}</div>;
     if (!launchUrl) return <div>No launch URL found</div>;
+
+    console.log(launchUrl);
 
     return (
         <div className={"container"}>
@@ -32,6 +39,10 @@ const App: React.FC = () => {
                 <h1>Odtwarzacz Szkoleń SCORM</h1>
                 <p>Prototyp w React/TypeScript do uruchamiania i śledzenia postępów.</p>
             </header>
+            {}
+            <div>
+
+            </div>
             <main>
                 { launchUrl.length > 0 &&
                 <ScormPlayer scormFilePath={'/scorm/'+launchUrl} /> }
